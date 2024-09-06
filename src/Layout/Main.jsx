6662361f7c2ca386b5a3fbf23f5af7f8f63cc6a9
Munir <smsirajulmonir@gmail.com>
2024-09-06 11:52:17 -0700

    navbar added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../Pages/Home/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div className='max-w-6xl mx-auto'>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
--- src/Pages/Home/Navbar/Navbar.jsx
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -1,5 +1,19 @@
+import { Link, NavLink } from 'react-router-dom';
+// import useAuth from '../../../hook/useAuth';
 
 const Navbar = () => {
+    let user=true;
+//   const { user, logOut } = useAuth();
+
+  const handleLogout = ()=>{
+    // logOut()
+  }
+    const links = 
+    <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/dashboard">Dashboard</NavLink></li>
+        <li><NavLink to="/notification">Notification</NavLink></li>
+    </>
     return (
         <div className="navbar bg-base-100">
   <div className="navbar-start">
@@ -21,36 +35,25 @@ const Navbar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        {links}
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">ParcelPilot</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      {links}
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+  {
+          user? <>
+          <button onClick={handleLogout} className='btn btn-ghost'>Logout</button>
+          </>:
+          <>
+            <Link to="/login"><button className='btn btn-ghost btn-sm'>Login</button></Link>
+          </>
+        }
   </div>
 </div>
     );
